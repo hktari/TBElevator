6f458970b8ed278f34e958abfafef132aa46ed39
hktari <bkamnik1995@gmail.com>
2020-10-18 10:50:15 +0200

    adds blinking led

--- TBElevator/TBElevator.ino
+++ TBElevator/TBElevator.ino
@@ -1,7 +1,7 @@
 /*
  * File: stepper_28byj_48_slim.ino
- * 
- * 28BYJ-48 stepper motor using the ULN2003 interface normally 
+ *
+ * 28BYJ-48 stepper motor using the ULN2003 interface normally
  * supplied with this stepper.
  *
  * Simple 28BYJ-48 Stepper Driver
@@ -9,7 +9,7 @@
  * Last Edit: 15/11/2018
  *
 # MIT License
-# 
+#
 # Copyright (c) 2018 Mark A Heywood
 # Author: Mark A Heywood
 # https://www.bluetin.io/
@@ -32,21 +32,21 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
  */
- 
+
 
 enum class ELEV_STATE
 {
-    IDLE,
-    CALIBRATION_STARTED,
-    CALIBRATION_IN_PROGRESS,
-    RUNNING,
+	IDLE,
+	CALIBRATION_STARTED,
+	CALIBRATION_IN_PROGRESS,
+	RUNNING,
 };
 enum class BTN_ACTION
 {
-    NONE = 0,
-    DOWN = 1,
-    UP = 2,
-    LONG_PRESS = 3
+	NONE = 0,
+	DOWN = 1,
+	UP = 2,
+	LONG_PRESS = 3
 };
 
 void HandleCalibBtn();
@@ -56,7 +56,7 @@ void phase8(bool isClockwise);
 //+++++++++++ Variables +++++++++++++++
 // Track current stepper phase by index
 int phaseIndex = 0;
- 
+
 //++++++++++++++ Initialise Constants ++++++++++++++++
 // Stepper port pin assignment. Default 0b11110000 on port D.
 const uint8_t STEPPERPORT = 0xF0;
@@ -64,13 +64,13 @@ const uint8_t STEPPERPORT = 0xF0;
 const uint8_t SERIALMASK = 0x03;
 // Motor 8-phase sequence lookup list
 // pins 7, 6, 5, 4 (0011, 0010, 0110, 0100, 1100, 1000, 1001, 0001)
-const uint8_t STEPPERPHASES8[8] = {0x30, 0x20, 0x60, 0x40, 0xC0, 0x80, 0x90, 0x10};
+const uint8_t STEPPERPHASES8[8] = { 0x30, 0x20, 0x60, 0x40, 0xC0, 0x80, 0x90, 0x10 };
 //+++++++++++++++++++++++++++++++++++++++++++++++++++++++
- 
- 
+
+
 const int CALIB_BTN_PIN = 13;
 const int STATE_LED_PIN = 11;
- 
+
 long timeNow;
 long timeInterval = 1200;
 bool rotateDirection = true;
@@ -86,162 +86,171 @@ int elevSteps = 0;
 int curStep = 0;
 bool moveDown;
 int ledState = LOW;
-
+long ledBlinkTimestamp = 0;
+const long LED_BLINK_SPEED = 500; // ms
 
 void SetState(ELEV_STATE state)
 {
-    Serial.println((int)state);
-    CurState = state;
+	Serial.println((int)state);
+	CurState = state;
 }
 
 void setup() {
-  // Set stepper pins to output
-  DDRD = DDRD | STEPPERPORT;
-  pinMode(CALIB_BTN_PIN, INPUT);
-  pinMode(STATE_LED_PIN, OUTPUT);
-  Serial.begin(9600);
+	// Set stepper pins to output
+	DDRD = DDRD | STEPPERPORT;
+	pinMode(CALIB_BTN_PIN, INPUT);
+	pinMode(STATE_LED_PIN, OUTPUT);
+	Serial.begin(9600);
 
-  SetState(ELEV_STATE::IDLE);
+	SetState(ELEV_STATE::IDLE);
 }
 
 void loop() {
-  HandleCalibBtn();
-  
-  if(CurCalibBtnAction == BTN_ACTION::LONG_PRESS)
-  {
-    SetState(ELEV_STATE::CALIBRATION_STARTED);
-  }
-  
-  switch(CurState)
-  {
-    case ELEV_STATE::IDLE:
-      if(CurCalibBtnAction == BTN_ACTION::UP && elevSteps != 0)
-      {
-        digitalWrite(STATE_LED_PIN, LOW);
-        SetState(ELEV_STATE::RUNNING);
-      }
-      break;
-    case ELEV_STATE::CALIBRATION_STARTED:
-      // Set LED high
-      digitalWrite(STATE_LED_PIN, HIGH);
-      // User lowers the lift to the ground and creates tension in the wire
-      // User presses the calibration button
-      if(CurCalibBtnAction == BTN_ACTION::DOWN)
-      {
-        elevSteps = curStep = 0;
-        SetState(ELEV_STATE::CALIBRATION_IN_PROGRESS);
-      }
-      break;
-    case ELEV_STATE::CALIBRATION_IN_PROGRESS:
-      // Set LED blinking ?
-      ledState = !ledState;
-      //digitalWrite(STATE_LED_PIN, ledState);
-      // Start moving up and count steps
-      if(tryMove(false))
-      {
-        elevSteps++;
-      }
+	HandleCalibBtn();
 
-      // User presses calibration button
-      if(CurCalibBtnAction == BTN_ACTION::DOWN)
-      {
-        curStep = elevSteps;
-        moveDown = true;
-        SetState(ELEV_STATE::RUNNING);
-      }
-      break;
-    case ELEV_STATE::RUNNING:
-      digitalWrite(STATE_LED_PIN, LOW);
-      
-      if(tryMove(moveDown))
-      {
-        curStep--;
-      }
+	if (CurCalibBtnAction == BTN_ACTION::LONG_PRESS)
+	{
+		SetState(ELEV_STATE::CALIBRATION_STARTED);
+	}
 
-      if(curStep == 0)
-      {
-        curStep = elevSteps;
-        moveDown = !moveDown;
-      }
+	switch (CurState)
+	{
+	case ELEV_STATE::IDLE:
+		if (CurCalibBtnAction == BTN_ACTION::UP && elevSteps != 0)
+		{
+			digitalWrite(STATE_LED_PIN, LOW);
+			SetState(ELEV_STATE::RUNNING);
+		}
+		break;
+	case ELEV_STATE::CALIBRATION_STARTED:
+		// Set LED high
+		digitalWrite(STATE_LED_PIN, HIGH);
+		// User lowers the lift to the ground and creates tension in the wire
+		// User presses the calibration button
+		if (CurCalibBtnAction == BTN_ACTION::DOWN)
+		{
+			elevSteps = curStep = 0;
+			SetState(ELEV_STATE::CALIBRATION_IN_PROGRESS);
+		}
+		break;
+	case ELEV_STATE::CALIBRATION_IN_PROGRESS:
+		auto now = millis();
+		if (now - ledBlinkTimestamp > LED_BLINK_SPEED)
+		{
+			ledState = !ledState;
+			ledBlinkTimestamp = now;
+			digitalWrite(STATE_LED_PIN, ledState);
+		}
 
-      if(CurCalibBtnAction == BTN_ACTION::DOWN)
-      {
-        SetState(ELEV_STATE::IDLE);
-      }
-      break;
-  }
-  
+		// Start moving up and count steps
+		if (tryMove(false))
+		{
+			elevSteps++;
+		}
 
-    
-  //turn90();
-  //PORTD = PORTD & SERIALMASK;   
-  //delay(30);
+		// User presses calibration button
+		if (CurCalibBtnAction == BTN_ACTION::DOWN)
+		{
+			curStep = elevSteps;
+			moveDown = true;
+			digitalWrite(STATE_LED_PIN, LOW);
+			SetState(ELEV_STATE::RUNNING);
+		}
+		break;
+	case ELEV_STATE::RUNNING:
+		if (tryMove(moveDown))
+		{
+			curStep--;
+		}
+
+		if (curStep == 0)
+		{
+			curStep = elevSteps;
+			moveDown = !moveDown;
+		}
+
+		if (CurCalibBtnAction == BTN_ACTION::DOWN)
+		{
+			SetState(ELEV_STATE::IDLE);
+		}
+		break;
+	}
+
+
+
+	//turn90();
+	//PORTD = PORTD & SERIALMASK;   
+	//delay(30);
 }
 void HandleCalibBtn()
 {
-  int calibBtnState = digitalRead(CALIB_BTN_PIN);
+	int calibBtnState = digitalRead(CALIB_BTN_PIN);
 
-  CurCalibBtnAction = BTN_ACTION::NONE;
-  
-  if(calibBtnState == HIGH && prevCalibBtnState != calibBtnState)
-  {
-    lastCalibBtnDown = millis();
-    longPressCondition = true;
-    CurCalibBtnAction = BTN_ACTION::DOWN;
-  }
-  else if(calibBtnState == LOW && prevCalibBtnState != calibBtnState)
-  {
-    longPressCondition = false;
-    CurCalibBtnAction = BTN_ACTION::UP;
-  }
-  else if(longPressCondition && (millis() - lastCalibBtnDown) > 3000)
-  {
-    CurCalibBtnAction = BTN_ACTION::LONG_PRESS;
-    longPressCondition = false;
-  }
-  
-  prevCalibBtnState = calibBtnState ;
+	CurCalibBtnAction = BTN_ACTION::NONE;
+
+	if (calibBtnState == HIGH && prevCalibBtnState != calibBtnState)
+	{
+		lastCalibBtnDown = millis();
+		longPressCondition = true;
+		CurCalibBtnAction = BTN_ACTION::DOWN;
+	}
+	else if (calibBtnState == LOW && prevCalibBtnState != calibBtnState)
+	{
+		longPressCondition = false;
+		CurCalibBtnAction = BTN_ACTION::UP;
+	}
+	else if (longPressCondition && (millis() - lastCalibBtnDown) > 3000)
+	{
+		CurCalibBtnAction = BTN_ACTION::LONG_PRESS;
+		longPressCondition = false;
+	}
+
+	prevCalibBtnState = calibBtnState;
 }
 
 
 bool tryMove(bool down)
 {
-  timeNow = micros();
-  if (timeNow - savedTime > timeInterval) {
-      phase8(down);
-      savedTime = micros();
-      return true;
-  } 
-  return false; 
+	timeNow = micros();
+	if (timeNow - savedTime > timeInterval) {
+		phase8(down);
+		savedTime = micros();
+		return true;
+	}
+	return false;
 }
 
-void phase8 (bool isClockwise) {
-  // Clear stepper pins
-  PORTD = PORTD & SERIALMASK;
- 
-  // Alter rotation direction
-  bool rotateDirection;
-  if(isClockwise == true) {
-    rotateDirection = true;
-  } else {
-    rotateDirection = false;
-  }
-  
-  if (rotateDirection == true) {
-    // Output the next stepper phase - Clockwise
-    PORTD = PORTD | STEPPERPHASES8[phaseIndex];
-    if (phaseIndex == 7) {
-      phaseIndex = 0;
-    } else {
-      phaseIndex++;
-    }
-  } else {
-    // Output the next stepper phase - Counter Clockwise
-    PORTD = PORTD | STEPPERPHASES8[phaseIndex];
-    if (phaseIndex == 0) {
-      phaseIndex = 7;
-    } else {  
-      phaseIndex--;
-    }
-  }
+void phase8(bool isClockwise) {
+	// Clear stepper pins
+	PORTD = PORTD & SERIALMASK;
+
+	// Alter rotation direction
+	bool rotateDirection;
+	if (isClockwise == true) {
+		rotateDirection = true;
+	}
+	else {
+		rotateDirection = false;
+	}
+
+	if (rotateDirection == true) {
+		// Output the next stepper phase - Clockwise
+		PORTD = PORTD | STEPPERPHASES8[phaseIndex];
+		if (phaseIndex == 7) {
+			phaseIndex = 0;
+		}
+		else {
+			phaseIndex++;
+		}
+	}
+	else {
+		// Output the next stepper phase - Counter Clockwise
+		PORTD = PORTD | STEPPERPHASES8[phaseIndex];
+		if (phaseIndex == 0) {
+			phaseIndex = 7;
+		}
+		else {
+			phaseIndex--;
+		}
+	}
 }
